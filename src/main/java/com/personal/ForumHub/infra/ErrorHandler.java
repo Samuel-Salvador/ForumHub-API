36d24f6f31be9af1fc9905463b33de4b5889dbbe
Samuel S <samuel20018@gmail.com>
2025-08-16 19:11:48 -0300

    created infra package and ErrorHandler class

---
+++ src/main/java/com/personal/ForumHub/infra/ErrorHandler.java
@@ -0,0 +1,30 @@
+package com.personal.ForumHub.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity error404Handler(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity error400Handler(MethodArgumentNotValidException exception){
+        var errors = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(ValidationErrorsData::new).toList());
+    }
+
+    private record ValidationErrorsData(String field,String message){
+
+        public ValidationErrorsData(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
