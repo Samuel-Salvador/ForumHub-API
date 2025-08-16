598c42ef33c635255222c6338199ce77589bff3b
Samuel S <samuel20018@gmail.com>
2025-08-16 15:57:00 -0300

    changed 1st migration

--- src/main/resources/db/migration/V1__create_table_topicos.sql
+++ src/main/resources/db/migration/V1__create_table_topicos.sql
@@ -1,9 +1,9 @@
 CREATE TABLE Topicos(
     id INTEGER PRIMARY KEY,
-    titulo VARCHAR(100),
+    titulo VARCHAR(100) NOT NULL,
     mensagem TEXT,
-    data_de_criacao DATE,
-    estado_do_topico VARCHAR(50),
-    autor VARCHAR(100),
-    curso VARCHAR(50)
+    data_de_criacao DATE NOT NULL,
+    estado_do_topico VARCHAR(50) NOT NULL,
+    autor VARCHAR(100) NOT NULL,
+    curso VARCHAR(50) NOT NULL
 );
