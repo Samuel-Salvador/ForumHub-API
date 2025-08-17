fd1c3e8ebb7b18fd3d5f1decd0790252beea8c41
Samuel S <samuel20018@gmail.com>
2025-08-17 11:04:31 -0300

    created UsuarioRepository interface

---
+++ src/main/java/com/personal/ForumHub/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package com.personal.ForumHub.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
